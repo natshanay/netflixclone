a32c2544c4a0f6b360b1352dee9c7059ee1e3ebe
Natnael Chala <natnaelchala10@gmail.com>
2024-09-03 07:25:46 -0700

    iam good feeling

---
+++ nana/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+
+
+function Home() {
+  return (
+    <div>
+
+        <Header/>
+        <Footer/>
+    </div>
+   
+  )
+}
+
+export default Home
